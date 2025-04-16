385dedfc46ee9def0b2bc3e4d0df7e95dc080d62
rucaua <tymofeiev.m@gmail.com>
2025-04-16 12:05:53 -0600

    responsive design for KeyFeatures

--- components/KeyFeaturesSection.vue
+++ components/KeyFeaturesSection.vue
@@ -1,9 +1,10 @@
 <template>
   <section class="container mx-auto py-20">
-    <div class="flex flex-row">
-      <div class="feature-card bg-gradient-to-br from-main to-secondary
-                  m-4 p-8 justify-between rounded-lg shadow-lg w-full flex flex-col items-center"
+    <div class="flex flex-row flex-wrap justify-between -mx-4">
+      <div class="flex flex-col flex-wrap w-full md:w-6/12 lg:w-3/12 px-4 my-4"
            v-for="(feature, index) in data" :key="index">
+        <div class="w-full feature-card bg-gradient-to-br from-main to-secondary p-8 rounded-lg shadow-lg
+        flex flex-col justify-center items-center">
         <div class="relative inline-block">
           <span class="bg-clip-text text-transparent bg-gradient-to-r from-alternative-bright to-secondary-bright text-5xl font-bold">
             {{ feature.title }}
@@ -11,6 +12,7 @@
           <span class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-alternative-bright to-secondary-bright"></span>
         </div>
         <span class="text-base mt-2 uppercase text-grey">{{feature.subTitle}}</span>
+      </div>
       </div>
     </div>
   </section>
