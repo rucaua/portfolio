8ecbeebe5826b1db4869dc6c47167ffe2f0369b8
rucaua <tymofeiev.m@gmail.com>
2025-04-22 13:35:51 -0600

    add Tags

---
+++ components/Tags.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3 class="text-xl font-semibold mb-4" v-if="title">{{ title }}</h3>
+    <div class="flex flex-wrap gap-2">
+            <span
+                v-for="tag in tags"
+                :key="tag.name"
+                class="px-3 py-2 text-base rounded-md border border-white/5 transition-all duration-300
+                  hover:-translate-y-0.5 hover:bg-secondary/10 hover:text-grey"
+                :class="tag.highlight ? 'bg-secondary/15 border-secondary/20' : 'bg-white/5'">
+              {{ tag.name }}
+            </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tags",
+  props: {
+    title: {
+      type: String,
+      default: null
+    },
+    tags: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
